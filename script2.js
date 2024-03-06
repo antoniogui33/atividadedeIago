3753b6451b6cf9e549f0bf0104da7f52b12f5087
antoniogui33 <156611747+antoniogui33@users.noreply.github.com>
2024-03-06 15:51:47 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,14 @@
+const meuArray = [1, 2, 3, 4, 5]
+const meuArray2 = ["sim", "nao", "talvez", "provavelmente", "não sei"]
+const meuArray3 = ["cigarro", "crack", "lol", "pornografia", "chocolate"]
+const meuArray4 = ['mae', 'pai', 'filho', 'aborto', 'policia militar']
+const meuArray5 = ['skylab', 'chico', 'marcelo', 'bolsa', 'cock']
+
+meuArray[2] = meuArray5[4]
+meuArray2[5] = meuArray4[2]
+meuArray3[6] = meuArray[4] 
+meuArray3[7] = meuArray4[4]
+meuArray3[8] = meuArray5 [4]
+meuArray3[9] = meuArray2[4]
+
+console.log(meuArray, meuArray2, meuArray3)
