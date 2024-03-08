3f7c05646b1d980036e82c507cd4caf4a587e376
antoniogui33 <156611747+antoniogui33@users.noreply.github.com>
2024-03-07 21:21:56 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,44 @@
+const meuObjeto = {
+    primeiraPropiedade: "sou a primeira propiedade.",
+    segundaPropiedade: "mentira, eu que sou."
+};
+
+//terceiro video logo em baixo, viu?
+
+//const meuObjet2 = []
+//const meuObjeto3 = {}
+
+//meuObjeto3.fruta = "tomate"
+//meuObjeto3[0] = "jabuticaba"
+
+//ultima parte!!!!
+
+function somaDoisNumeros(numero1, numero2){
+    return numero1 + numero2;
+}
+console.log(somaDoisNumeros(5, 10))
+
+somaDoisNumeros(5, 10)
+
+//atividade
+
+function decodificarAtividade(variaveComTexto){ 
+    let primeiraDecodificaao = variavelComTexo.split(',').toString()
+    let segundaDecodificacao = primeiraDecodificacao.split(',')
+    let terceiraDecodificacao = segundaDecodificacao.filter(variaveComTexto); {
+        if (primeiraDecodificaao = ","){
+            return true
+        }
+        if(segundaDecodificacao = ","){
+            return true}
+        
+        return Number.isNaN(Number(terceiraDecodificacao))
+    }
+}
+let primeiraDecodificacao = segundaDecodificacao.join('4')
+
+console.log(variaveComTexto)
+
+
+decodificarAtividade(atividadeCodificada)
+
